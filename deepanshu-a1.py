ccef0d98f00083f695da2c704db1d747d29c47fe
Nishan <nisanstha355@gmail.com>
2025-02-12 17:18:41 -0500

    Welcome greeting

---
+++ deepanshu-a1.py
@@ -0,0 +1,9 @@
+# Main program for Arnold's Amazing Eats Ordering System starts here
+#this while loop will keep running until the user confirms the order
+  
+while True:
+    print(" Welcome to Arnold's Amazing Eats Ordering System! ")
+    print("=" * 50)
+    print("We offer delicious meals delivered to your doorstep.")
+    print("Please follow the prompts to place your order.")
+    print("-" * 50)
